4107621501d5dea2851e45a44f7dd536be4f71ef
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-03-01 02:59:56 +0600

    Expense tracker without any databse/local storage

---
+++ main.js
@@ -0,0 +1,51 @@
+function getId(id) {
+  return document.getElementById(id);
+}
+const expenseName = getId("expense-name");
+const expenseAmount = getId("expense-amount");
+const expenseType = getId("expense-type");
+const expenseResult = getId("expense-result");
+
+document.getElementById("expense-add-btn").addEventListener("click", () => {
+  const nameVal = expenseName.value;
+  const amountVal = expenseAmount.value;
+  const typeVal = expenseType.value;
+  console.log(typeVal === "Entry Type");
+  if (!nameVal || !amountVal || typeVal === "Entry Type") {
+    alert(
+      "⚠️ All fields are required!\nPlease fill out all inputs before proceeding.",
+    );
+    return;
+  }
+  addToIncome(typeVal, nameVal, amountVal);
+});
+
+function addToIncome(typeVal, nameVal, amountVal) {
+  const resultCard = document.createElement("div");
+  const cardClasses = ["card", "w-full", "py-2", "bg-white", "border", "px-4"];
+  const badgeClasses = ["badge", "text-white"];
+  if (typeVal === "Income") {
+    cardClasses.push("border-purple-500", "text-purple-500");
+    badgeClasses.push("bg-purple-500");
+  } else {
+    cardClasses.push("border-secondary", "text-secondary");
+    badgeClasses.push("badge-secondary");
+  }
+  document.querySelector(".no-expense").classList.add("hidden");
+  resultCard.classList.add(...cardClasses);
+  resultCard.innerHTML = `
+       <h1 class="flex items-center justify-between"> <span> ${nameVal} <strong>${amountVal}</strong> tk  </span>   <span class="${badgeClasses.join(" ")}">${typeVal}</span> <i class="fas fa-trash cursor-pointer dlt-expense"></i>  </h3>
+   `;
+  expenseResult.appendChild(resultCard);
+  expenseName.value = "";
+  expenseAmount.value = "";
+  expenseType.value = "Entry Type";
+}
+expenseResult.addEventListener("click", (e) => {
+  const dltBtn = e.target.closest(".dlt-expense");
+  if (!dltBtn) return;
+  expenseResult.removeChild(dltBtn.closest(".card"));
+  if (expenseResult.children.length <= 1) {
+    document.querySelector(".no-expense").classList.remove("hidden");
+  }
+});
